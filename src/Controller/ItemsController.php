db00c9d6bba2bdc2b078b1345c586016cc253a6f
marzepanh <marzepanh@gmail.com>
2022-06-26 22:47:46 +0700

    Some changes

--- src/Controller/ItemsController.php
+++ src/Controller/ItemsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Item;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ItemsController extends AbstractController
 {
     #[Route('/items/{id}', name: 'app_items')]
-    public function index(int $id): Response
+    public function index(Item $item): Response
     {
         return $this->render('items/index.html.twig', [
-            'controller_name' => 'ItemsController ' . $id,
+            'controller_name' => 'ItemsController',
+            'item' => $item
         ]);
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ItemRepository $itemRepository): Response
     {
+        $items = $itemRepository->findAll();
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'items' => $items
         ]);
     }
 }
